251336837205557eadbeb231a618d97f2d786fc9
eresyyl <eresyyl@gmail.com>
2014-09-24 13:22:15 +0700

    events!
    events!

--- trunk/wp-config.php
+++ trunk/wp-config.php
@@ -16,16 +16,16 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'u197274629_esagu');
+define('DB_NAME', 'mk');
 
 /** MySQL database username */
-define('DB_USER', 'u197274629_abera');
+define('DB_USER', 'mk');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'qwerty123');
+define('DB_PASSWORD', 'qwerty1');
 
 /** MySQL hostname */
-define('DB_HOST', 'mysql.hostinger.ru');
+define('DB_HOST', 'localhost');
 
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
